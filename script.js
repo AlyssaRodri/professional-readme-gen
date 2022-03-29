9c58b5b3ae39c8aa8c3e3cd07a3b848057854519
Alyssa Rodriguez <alyssa.taylor.rodriguez@gmail.com>
2022-03-28 21:13:03 -0500

    ReadMe template completed.

--- script.js
+++ script.js
@@ -102,21 +102,77 @@ const askUser = () => {
             name: 'licenseType',
             message: 'Which licensae would you like to use?',
             choices: licenseChoices
+        },
+        {
+            //Contributions
+            type: 'input',
+            name:'contributions',
+            message: 'Please give us a list of all resources used for your project, and credit to other collaborators. '
+
+        },
+        {
+            //Tests
+            type: 'input',
+            name: 'tests',
+            message: 'Please explain all tests that a user should perform in your application'
+        },
+        {
+            //Github
+            type: 'input',
+            name: 'github',
+            message: 'Please provide a link to your GitHub'
+        },
+        {
+            //Email
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
         }
     ])
 }
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
+
+const generateReadMe = ({ title, description, installation, usage, license, contributions, tests, github, email }) => 
+`
+ # ${title}
+
+ ## Description
+
+ ${description}
+
+ ## Table of Contents
+ - [Installation](#installation)
+ - [Usage](#usage)
+ - [Contributions](#contributions)
+ - [Test](#tests)
+ - [Questions](#questions)
+
+ ## Installation
+
+ ${installation}
+
+ ## Usage
+
+ ${usage}
+
+ ## Contributions
+
+ ${contributions}
+
+ ## Tests
+
+ ${tests}
+
+ ## Questions
+ 
+ If you have any questions about my work, please feel free to contect me at ${email} or [visit my Github](${github})
+`
+
+
+//Here I called our function
+const runPrompt = () => {
+    askUser()
+    .then((answers) => fs.writeFileSync('READMETest.md', generateReadMe(answers)))
+    .catch((err) => console.error(err));
+}
+
+runPrompt()
